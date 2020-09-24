133b466a9ce8e500b7dad6467f593d0a8d49684a
ron nachmany <rnachmany@anyvision.co>
2020-09-24 17:47:16 +0300

    init icons

---
+++ dist/ArrowSolidRight.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+function SvgArrowSolidRight(props) {
+  return (
+    <svg width={24} height={24} {...props}>
+      <path d="M15.318 11.249a1 1 0 010 1.505l-4.339 3.8a1 1 0 01-1.658-.752v-7.6a1 1 0 011.657-.749z" />
+    </svg>
+  );
+}
+
+export default SvgArrowSolidRight;
---
+++ dist/Cancel.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+function SvgCancel(props) {
+  return (
+    <svg width={16} height={16} {...props}>
+      <path
+        d="M9.131 8.001l2.634-2.634a.8.8 0 10-1.131-1.131L8.001 6.869 5.366 4.235a.8.8 0 10-1.131 1.131l2.634 2.635-2.634 2.633a.8.8 0 101.131 1.131l2.635-2.634 2.634 2.634a.8.8 0 101.131-1.131z"
+        data-name="Path 2223"
+      />
+    </svg>
+  );
+}
+
+export default SvgCancel;
